c06b8ace879e9c204f19615978fc8d6a6c3ae8f1
Shubham Kumar <shubham2270@gmail.com>
2019-07-02 17:44:28 +0530

    worked on register routing

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const knex = require('knex');
 const db = knex({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : 'dummypassword',
-      database : 'book_store'
-}
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'dummypassword',
+        database: 'book_store'
+    }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data)
-});
+// db.select('*').from('users').then(data => {
+//     console.log(data)
+// });
 
 app.use(express.json());
 app.use(cors());
@@ -47,50 +47,64 @@ const database = {
 
 app.post('/login', (req, res) => {
 
-    bcrypt.compare('Ann', '$2b$10$j6xuACF3/yW9En3nRCEI4.GRm7gxCMh0mGCbMsvoBRnECU73vqc4W', function(err, res) {
-        console.log('first guess',res)
+    bcrypt.compareSync('Ann', '$2b$10$j6xuACF3/yW9En3nRCEI4.GRm7gxCMh0mGCbMsvoBRnECU73vqc4W', function (err, res) {
+        console.log('first guess', res)
     });
-    bcrypt.compare('ann', '$2b$10$j6xuACF3/yW9En3nRCEI4.GRm7gxCMh0mGCbMsvoBRnECU73vqc4W', function(err, res) {
+    bcrypt.compareSync('ann', '$2b$10$j6xuACF3/yW9En3nRCEI4.GRm7gxCMh0mGCbMsvoBRnECU73vqc4W', function (err, res) {
         console.log('second guess', res)
     });
 
-    if(req.body.email === database.users[0].email &&
-       req.body.password === database.users[0].password) {
-           res.json('Login Success')
-       } else {
-           res.status(400).json('Error logging in')
-       }
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('Login Success')
+    } else {
+        res.status(400).json('Error logging in')
+    }
 })
 
 app.post('/register', (req, res) => {
-    const { email, name, password } = req.body
-    db('users')
-    .insert([{
-        email: email,
-        name: name,
-        joined: new Date()
-    }]).then(response => {
-        res.json(response)
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            // res.json(email)
+            .then(loginEmail => {
+                return trx('users')
+                    // .returning('*')
+                    .insert([{
+                        email: loginEmail,
+                        name: name,
+                        joined: new Date()
+                    }])
+                    .then(response => {
+                        res.json(response)
+                        console.log(response)
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
-  
+        .catch(err => res.status(400).json('unable to register'))
+
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-           return res.json(user)
-        }
-    })
-    if(!found) {
-        res.status(400).json('not found')
-    }
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not found')
+            }
+        })
+        .catch(err => res.status(400).json('error getting user'))
 })
 
 app.listen(4000, () => {
     console.log('App is running on port 4000');
 })
- 
